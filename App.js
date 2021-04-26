5ee357a8c8c96cfaa29f9e84bbe1119e06007aea
marville001 <mwangimartin1904@gmail.com>
2021-04-26 10:03:56 -0400

    Added firebase

--- App.js
+++ App.js
@@ -1,11 +1,24 @@
-import React from 'react';
-import DrawerNavigation from './app/Navigation/DrawerNavigation';
-import Login from './app/screens/Login';
+import React from "react";
+import { useState } from "react";
+import { View } from "react-native";
+import DrawerNavigation from "./app/Navigation/DrawerNavigation";
+import Login from "./app/scenes/Login";
+
+import * as firebase from 'firebase';
+import apiKeys from './app/config/firebaseConfig'
 
 export default function App() {
-  return (
-    // <DrawerNavigation />
-    <Login/>
-  );
-}
+  const [isLoaded, setIsLoaded] = useState(false);
+  const [isAuthenticated, setIsAuthenticated] = useState(true);
+  const [isAuthenticationReady, setIsAuthenticationReady] = useState(false);
 
+  if(!firebase.apps.length){
+    firebase.initializeApp(apiKeys);
+    firebase.auth().onAuthStateChanged(user=>{
+        setIsAuthenticationReady(true);
+        setIsAuthenticated(!!user);
+    })
+  }
+
+  return <>{isAuthenticated ? <DrawerNavigation /> : <Login />}</>;
+}
---
+++ app/config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyBXC9U7DkjEO3VEkRR9Zk1HyOPqp7u2o94",
+  authDomain: "smartsign-a6126.firebaseapp.com",
+  projectId: "smartsign-a6126",
+  storageBucket: "smartsign-a6126.appspot.com",
+  messagingSenderId: "677899350871",
+  appId: "1:677899350871:web:e367d45426cb3e1a8b8412",
+  measurementId: "G-FNTKDQWF13",
+};
