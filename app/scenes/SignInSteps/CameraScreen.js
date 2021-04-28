f355cf57c77af9fdd15dfc732a0a51c0341af378
marville001 <mwangimartin1904@gmail.com>
2021-04-28 16:53:52 -0400

    Added Camera Screen

--- app/scenes/SignInSteps/CameraScreen.js
+++ app/scenes/SignInSteps/CameraScreen.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
 import { Camera } from "expo-camera";
+// import { Icon } from "react-native-elements";
+import { Ionicons } from "@expo/vector-icons";
 
-const CameraScreen = () => {
+const CameraScreen = ({navigation}) => {
   const [hasPermission, setHasPermission] = useState(null);
   const [type, setType] = useState(Camera.Constants.Type.back);
 
@@ -33,7 +35,27 @@ const CameraScreen = () => {
               );
             }}
           >
-            <Text style={styles.text}> Flip </Text>
+            <Text style={styles.text}> Capture </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={[styles.captureButton, styles.button]}
+            onPress={() => {
+              setType(
+                type === Camera.Constants.Type.back
+                  ? Camera.Constants.Type.front
+                  : Camera.Constants.Type.back
+              );
+            }}
+          >
+            {/* <Icon name="rowing" /> */}
+            <Ionicons name="camera" size={35} color="white" />
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => navigation.navigate("Home")}
+          >
+            {/* <Icon name="rowing" /> */}
+            <Ionicons name="cancel" size={35} color="white" />
           </TouchableOpacity>
         </View>
       </Camera>
@@ -50,15 +72,24 @@ const styles = StyleSheet.create({
   },
   buttonContainer: {
     flex: 1,
+    position: "absolute",
+    bottom: 20,
+    left: 20,
+    right: 20,
     backgroundColor: "transparent",
     flexDirection: "row",
+    justifyContent: "space-between",
     margin: 20,
   },
   button: {
-    flex: 0.1,
-    alignSelf: "flex-end",
-    alignItems: "center",
+    justifyContent: "center",
+    alignItems:"center",
+    width: 50,
+    height: 50,
+    backgroundColor: "#eee",
+    borderRadius:50
   },
+  captureButton: {},
   text: {
     fontSize: 18,
     color: "white",
