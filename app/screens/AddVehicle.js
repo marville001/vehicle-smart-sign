2f1b4d5c5f679536fb8fa4c494c023a57c6ac669
marville001 <mwangimartin1904@gmail.com>
2021-05-17 15:43:10 +0300

    Update

--- app/screens/AddVehicle.js
+++ app/screens/AddVehicle.js
@@ -1,17 +1,63 @@
 import React from "react";
 
-import { View, StyleSheet, Button, Text, StatusBar } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import Header from "../components/Header";
+import {
+  View,
+  StyleSheet,
+  Button,
+  Text,
+  TextInput,
+  StatusBar,
+} from "react-native";
+import { ScrollView } from "react-native-gesture-handler";
 import { colors } from "../constants/theme";
-import { Feather as Icon } from "@expo/vector-icons";
 
 const AddVehicle = ({ navigation }) => {
   return (
     <View style={styles.container}>
       <StatusBar />
-      <View>
-          <Text>Add Vehicle Record</Text>
+      <View
+        style={{
+          display: "flex",
+          alignItems: "center",
+          padding: 30,
+        }}
+      >
+        <Text
+          style={{
+            fontSize: 20,
+          }}
+        >
+          Add Vehicle Record
+        </Text>
+      </View>
+
+      <View style={styles.form}>
+        <ScrollView>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Model</Text>
+            <TextInput style={styles.input} placeholder="Model..." />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Color</Text>
+            <TextInput style={styles.input} placeholder="Email" />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Number Plate</Text>
+            <TextInput style={styles.input} placeholder="Email" />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Enter Email</Text>
+            <TextInput style={styles.input} placeholder="Email" />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Enter Email</Text>
+            <TextInput style={styles.input} placeholder="Email" />
+          </View>
+          <View style={styles.inputContainer}>
+            <Text style={styles.label}>Enter Email</Text>
+            <TextInput style={styles.input} placeholder="Email" />
+          </View>
+        </ScrollView>
       </View>
     </View>
   );
@@ -20,7 +66,24 @@ const AddVehicle = ({ navigation }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: colors.primary
+    backgroundColor: colors.primary,
+  },
+  form: {
+    flex: 1,
+    paddingHorizontal: 30,
+    flexDirection: "row"
+  },
+  inputContainer: {
+    marginVertical: 10,width:"50%"
+  },
+  input: {
+    borderWidth: 1,
+    height: 40,
+    paddingLeft: 10,
+  },
+  label: {
+    fontSize: 15,
+    marginBottom: 10,
   },
 });
 
