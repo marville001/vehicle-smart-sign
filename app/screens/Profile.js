9e0d2f4c9e202d20610b925ad2f932bd8767791f
marville001 <mwangimartin1904@gmail.com>
2021-09-14 19:07:26 +0300

    Updated profile details

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -1,67 +1,54 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 
-import { View, StyleSheet, Button, Text, StatusBar } from "react-native";
-import { TouchableOpacity } from "react-native-gesture-handler";
-import Header from "../components/Header";
+import { View, StyleSheet, Image, TouchableOpacity, Button, Text, StatusBar } from "react-native";
+import { db } from "../../firebase";
 import { colors } from "../constants/theme";
-import { Feather as Icon } from "@expo/vector-icons";
+import AuthContext from "../provider/AuthProvider";
 
-// const Profile = ({ navigation }) => {
-//   return (
-//     <View style={styles.container}>
-//       <StatusBar />
-//       <View style={styles.header}>
+const Profile = () => {
+  const {user} = useContext(AuthContext)
+
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        let userRef = db.ref('users/' + user?.uid);
+        console.log(userRef);
+        await userRef.once('value', (snap)=>{
+          console.log(snap.val());
+        });
+
         
-//       </View>
-//       <View style={styles.content}>
-//         <Text style={styles.text}>Profile is here</Text>
-//         <Button title="Go Home" onPress={() => navigation.goBack()} />
-//       </View>
-//     </View>
-//   );
-// };
+      } catch (error) {
+        console.log(error);
+      }
+    }
 
-const profile = ({navigation})=>{
+    getUser();
+  },[user])
   return (
     <View style={styles.container}>
-          <View style={styles.header}></View>
-          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
-          <View style={styles.body}>
-            <View style={styles.bodyContent}>
-              <Text style={styles.name}>John Doe</Text>
-              <Text style={styles.info}>UX Designer / Mobile developer</Text>
-              <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
-              
-              <TouchableOpacity style={styles.buttonContainer}>
-                <Text>Opcion 1</Text>  
-              </TouchableOpacity>              
-              <TouchableOpacity style={styles.buttonContainer}>
-                <Text>Opcion 2</Text> 
-              </TouchableOpacity>
-            </View>
+      <View style={styles.header}></View>
+      <Image style={styles.avatar} source={{ uri: 'https://bootdey.com/img/Content/avatar/avatar6.png' }} />
+      <View style={styles.body}>
+        <View style={styles.bodyContent}>
+          <Text style={styles.name}>Martin Mwangi</Text>
+          <Text style={styles.info}>UX Designer / Mobile developer</Text>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text>{user?.email}</Text>
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.buttonContainer}>
+            <Text>{user.uid}</Text>
+          </TouchableOpacity>
         </View>
       </View>
+    </View>
   )
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  // },
-  // content: {
-  //   flex: 1,
-  //   alignItems: "center",
-  //   justifyContent: "center",
-  //   backgroundColor: colors.secondary,
-  // },
-  // text: {
-  //   fontSize: 20,
-  //   color: "#ffffff",
-  //   fontWeight: "800",
-  // },
-  header:{
-    backgroundColor: "#00BFFF",
-    height:200,
+  header: {
+    backgroundColor: colors.secondary,
+    height: 200,
   },
   avatar: {
     width: 130,
@@ -69,49 +56,43 @@ const styles = StyleSheet.create({
     borderRadius: 63,
     borderWidth: 4,
     borderColor: "white",
-    marginBottom:10,
-    alignSelf:'center',
+    marginBottom: 10,
+    alignSelf: 'center',
     position: 'absolute',
-    marginTop:130
+    marginTop: 130
   },
-  name:{
-    fontSize:22,
-    color:"#FFFFFF",
-    fontWeight:'600',
+  name: {
+    fontSize: 22,
+    color: "#FFFFFF",
+    fontWeight: '600',
   },
-  body:{
-    marginTop:40,
+  body: {
+    marginTop: 40,
   },
   bodyContent: {
     flex: 1,
     alignItems: 'center',
-    padding:30,
+    padding: 30,
   },
-  name:{
-    fontSize:28,
+  name: {
+    fontSize: 28,
     color: "#696969",
     fontWeight: "600"
   },
-  info:{
-    fontSize:16,
+  info: {
+    fontSize: 16,
     color: "#00BFFF",
-    marginTop:10
-  },
-  description:{
-    fontSize:16,
-    color: "#696969",
-    marginTop:10,
-    textAlign: 'center'
+    marginTop: 10
   },
   buttonContainer: {
-    marginTop:10,
-    height:45,
+    marginTop: 20,
+    height: 45,
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    marginBottom:20,
-    width:250,
-    borderRadius:30,
+    marginBottom: 0,
+    width: 300,
+    borderRadius: 20,
     backgroundColor: "#00BFFF",
   },
 });
