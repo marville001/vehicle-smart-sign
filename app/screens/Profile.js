f2f953efa1155e90bdee2af7ee6350c2e5c2ffbf
marville001 <mwangimartin1904@gmail.com>
2021-09-14 18:46:16 +0300

    Added profile page

--- app/screens/Profile.js
+++ app/screens/Profile.js
@@ -6,37 +6,114 @@ import Header from "../components/Header";
 import { colors } from "../constants/theme";
 import { Feather as Icon } from "@expo/vector-icons";
 
-const Profile = ({ navigation }) => {
+// const Profile = ({ navigation }) => {
+//   return (
+//     <View style={styles.container}>
+//       <StatusBar />
+//       <View style={styles.header}>
+        
+//       </View>
+//       <View style={styles.content}>
+//         <Text style={styles.text}>Profile is here</Text>
+//         <Button title="Go Home" onPress={() => navigation.goBack()} />
+//       </View>
+//     </View>
+//   );
+// };
+
+const profile = ({navigation})=>{
   return (
     <View style={styles.container}>
-      <StatusBar />
-      <View style={styles.header}>
-        
+          <View style={styles.header}></View>
+          <Image style={styles.avatar} source={{uri: 'https://bootdey.com/img/Content/avatar/avatar6.png'}}/>
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+              <Text style={styles.name}>John Doe</Text>
+              <Text style={styles.info}>UX Designer / Mobile developer</Text>
+              <Text style={styles.description}>Lorem ipsum dolor sit amet, saepe sapientem eu nam. Qui ne assum electram expetendis, omittam deseruisse consequuntur ius an,</Text>
+              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Opcion 1</Text>  
+              </TouchableOpacity>              
+              <TouchableOpacity style={styles.buttonContainer}>
+                <Text>Opcion 2</Text> 
+              </TouchableOpacity>
+            </View>
+        </View>
       </View>
-      <View style={styles.content}>
-        <Text style={styles.text}>Profile is here</Text>
-        <Button title="Go Home" onPress={() => navigation.goBack()} />
-      </View>
-    </View>
-  );
-};
+  )
+}
 
 const styles = StyleSheet.create({
-  container: {
+  // container: {
+  //   flex: 1,
+  // },
+  // content: {
+  //   flex: 1,
+  //   alignItems: "center",
+  //   justifyContent: "center",
+  //   backgroundColor: colors.secondary,
+  // },
+  // text: {
+  //   fontSize: 20,
+  //   color: "#ffffff",
+  //   fontWeight: "800",
+  // },
+  header:{
+    backgroundColor: "#00BFFF",
+    height:200,
+  },
+  avatar: {
+    width: 130,
+    height: 130,
+    borderRadius: 63,
+    borderWidth: 4,
+    borderColor: "white",
+    marginBottom:10,
+    alignSelf:'center',
+    position: 'absolute',
+    marginTop:130
+  },
+  name:{
+    fontSize:22,
+    color:"#FFFFFF",
+    fontWeight:'600',
+  },
+  body:{
+    marginTop:40,
+  },
+  bodyContent: {
     flex: 1,
+    alignItems: 'center',
+    padding:30,
   },
-  content: {
-    flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
-    backgroundColor: colors.secondary,
+  name:{
+    fontSize:28,
+    color: "#696969",
+    fontWeight: "600"
   },
-  text: {
-    fontSize: 20,
-    color: "#ffffff",
-    fontWeight: "800",
+  info:{
+    fontSize:16,
+    color: "#00BFFF",
+    marginTop:10
   },
-  button: {},
+  description:{
+    fontSize:16,
+    color: "#696969",
+    marginTop:10,
+    textAlign: 'center'
+  },
+  buttonContainer: {
+    marginTop:10,
+    height:45,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom:20,
+    width:250,
+    borderRadius:30,
+    backgroundColor: "#00BFFF",
+  },
 });
 
 export default Profile;
